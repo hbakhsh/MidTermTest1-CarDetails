bcd854517a4233bc6934b2a76e136ce5e496a6a2
hbakhsh <hbakhsh1@my.centennialcollege.ca>
2022-10-25 13:31:43 -0400

    First codes

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //local MongoDB deployment ->
+  URI: "mongodb+srv://hbakhshi:vfaSnLWPzuEDebVe@myfirstcluster.652welx.mongodb.net/car_store?retryWrites=true&w=majority",
+  Secret: "SomeSecret",
+};
+
